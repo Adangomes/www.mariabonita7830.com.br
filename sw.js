57b487501d58bdd982744ac48543e2465c7f45f9
Adan Gomes <adangones55@gmail.com>
2026-02-16 14:56:51 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,21 @@
 const CACHE_NAME = 'mydi-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './imagem/icon-192.png',
+  './imagem/icon-512.png'
+];
 
 self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
   self.skipWaiting();
 });
 
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    fetch(e.request).catch(() => {
-      return caches.match(e.request);
-    })
+    caches.match(e.request).then((res) => res || fetch(e.request))
   );
 });
